f40326595286ff4d0f241652f2c17096d92d2ccd
Tusharparihar <143849896+Tusharparihar@users.noreply.github.com>
2025-10-03 11:05:54 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.5.0, < 1.8.0"
+}
+
+provider "azurerm" {
+  version = "~> 3.80.0"
+}
+
+provider "random" {
+  version = ">= 3.5.0, < 3.8.0"
+}
+
+provider "null" {
+  version = ">= 3.2.0, < 3.5.0"
+}
+
+provider "local" {
+  version = ">= 2.4.0, < 2.6.0"
+}
